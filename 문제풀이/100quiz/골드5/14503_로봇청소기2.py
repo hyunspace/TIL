07ce73dc57992d6eab5910552d0503aedfc231c8
hyunspace <at.hyunspace@gmail.com>
2022-04-14 02:11:58 +0900

    22-04-14 CSS

--- 문제풀이/100quiz/골드5/14503_로봇청소기2.py
+++ 문제풀이/100quiz/골드5/14503_로봇청소기2.py
@@ -1,41 +1,37 @@
-from collections import deque
 from pprint import pprint
 
 N, M = map(int, input().split())
 r, c, d = map(int, input().split())
 rooms = [list(map(int, input().split())) for _ in range(N)]
+# cleaned = [[0] * M for _ in range(N)]
 
-'''
-queue에 들어가 있는 애들 => 청소 후보 
-'''
+ans = 1 # 시작 위치는 청소 완료!
+turns = 0
+dr, dc = [-1, 0, 1, 0], [0, 1, 0, -1]
 
-ans = 0
-queue = deque()
-queue.append((r, c, d)) # 현재 위치, 방향, 횟수
+while True:
+    # 왼쪽으로 회전 후
+    d = (d + 3) % 4
+    # 앞으로 한 칸
+    nr, nc = r + dr[d], c + dc[d]
+    # 아직 청소 X + 청소 가능한 곳?
+    if rooms[nr][nc] == 0:
+        rooms[nr][nc] = 2 # 청소 완료
+        ans += 1          # 청소 했으니까 세기
+        turns = 0         # 회전 횟수 초기화(2a에서 1번으로 돌아갔으므로)
+        r, c = nr, nc     # 현재 위치로 바꿔줌
+        continue
+    # 청소 불가능한 곳 => 돌아야...
+    else:
+        turns += 1
 
-dy, dx = [0, -1, 0, 1], [-1, 0, 1, 0]
-
-while queue:
-    y, x, di = queue.popleft()
-
-    if rooms[y][x] == 0: # 청소 가능한 곳이면
-        ans += 1
-        rooms[y][x] = 2  # 청소 표시
-
-    for i in range(4):
-        if i < 3: # 3이하일 때는 여러번 시도 괜찮음
-            nd = (di + 3) % 4
-            ny, nx = y + dy[nd], x + dx[nd] # 현재 위치의 바로 왼쪽
-            if 0 <= ny < N and 0 <= nx < M and rooms[ny][nx] == 0:
-                queue.append((ny, nx, nd))
-                break
-            else:
-                di = (di + 3) % 4   # 왼쪽 회전만
-        elif i == 3: # 다 채웠다! => 뒤를 확인 해야함
-            back = (di + 3) % 4
-            by, bx = y + dy[back], x + dx[back]
-            if 0 <= by < N and 0 <= bx < M and rooms[by][bx] == 0:
-                queue.append((by, bx, di))
-            else:
-                break
+    if turns == 4: # 돌만큼 돌았다
+        # 후진하자
+        d = (d + 2) % 4
+        nr, nc = r + dr[d], c + dc[d]
+        if rooms[nr][nc] != 1: # 벽이 아님 => 후진 가능!
+            r, c = nr, nc
+        else: # 후진해도 더이상 청소 불가능
+            break
+        turns = 0
 print(ans)
