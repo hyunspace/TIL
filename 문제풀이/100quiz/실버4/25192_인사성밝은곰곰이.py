02812ed5e4029db3319fda69bfc81fa5839b1bf3
hyunspace <at.hyunspace@gmail.com>
2022-05-20 01:15:18 +0900

    22-05-19 Algorithm

--- 문제풀이/100quiz/실버4/25192_인사성밝은곰곰이.py
+++ 문제풀이/100quiz/실버4/25192_인사성밝은곰곰이.py
@@ -1,19 +1,37 @@
 # 5월 15일 시간초과
 
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
+#
+# N = int(input())
+# users = []
+# cnt = 0
+# for _ in range(N):
+#     user = input()
+#     if user == 'ENTER':
+#         users = []
+#     else:
+#        if user in users:
+#            continue
+#        else:
+#            users.append(user)
+#            cnt += 1
+# print(cnt)
+
+#######################################################
+
+# 5월 19일
 
 N = int(input())
-users = []
+chatting = set()
 cnt = 0
+
 for _ in range(N):
-    user = input()
-    if user == 'ENTER':
-        users = []
+    chat = input()
+    if chat == 'ENTER':
+        cnt += len(chatting)
+        chatting = set()
     else:
-       if user in users:
-           continue
-       else:
-           users.append(user)
-           cnt += 1
+        chatting.add(chat)
+cnt += len(chatting)
 print(cnt)
